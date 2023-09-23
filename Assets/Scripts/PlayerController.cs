06d2936728011f16e0da438c9443ba7f8b9d1a0f
Hikmet Uygur <uygurhikmet@gmail.com>
2023-09-23 16:18:13 +0300

    Added flip player sprite.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,21 +6,57 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] private float playerSpeed = 5f;
+    [SerializeField] private float jumpSpeed = 20f;
+
+    private Rigidbody2D rigidbody;
+    private Animator animator;
+    
     Vector2 moveInput;
+    
     void Start()
     {
-        
+        rigidbody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     
     void Update()
     {
+        Run();
+        FlipSprite();
         
     }
 
     void OnMove(InputValue value)
     {
         moveInput = value.Get<Vector2>();
-        Debug.Log(moveInput);
+    }
+
+    void Run()
+    {
+        Vector2 moveVelocity = new Vector2(moveInput.x * playerSpeed, rigidbody.velocity.y);
+        rigidbody.velocity = moveVelocity;
+    }
+
+    void FlipSprite()
+    {
+        bool hasHorizontalSpeed = Mathf.Abs(rigidbody.velocity.x) > Mathf.Epsilon;
+        animator.SetBool("isRunning",false);
+        if (hasHorizontalSpeed)
+        {
+            transform.localScale = new Vector2(Mathf.Sign(rigidbody.velocity.x), 1f);
+            animator.SetBool("isRunning", true);
+        }
+        
+    }
+    void OnJump(InputValue value)
+    {
+        
+
+        if (value.isPressed)
+        {
+            rigidbody.velocity = new Vector2(rigidbody.velocity.x, jumpSpeed);
+        }
     }
 }
