d5a27c7c140fde5db0bd21ae98753418fb6511a5
Hikmet Uygur <uygurhikmet@gmail.com>
2023-09-27 19:39:26 +0300

    Create some levels, Add level exit portal.

---
+++ Assets/Scripts/LevelExit.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelExit : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        var currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+}
